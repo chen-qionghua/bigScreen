0ebf8be1d461ab43b1ef5f3fa936cd1d7f52a279
chengqionghua <1169121469@qq.com>
2022-04-08 08:53:39 +0800

    初步实现chart2

--- src/components/chart-2.tsx
+++ src/components/chart-2.tsx
@@ -9,27 +9,29 @@ export const Chart2 = () => {
   useEffect(() => {  let myChart = echarts.init(divRef.current);
     myChart.setOption(createEchartOptions({
 
-      axisLabel:{
-        formatter(val){
-          if(val.length > 2){
-            const reallyVal = val.substring(0,3) +'\n' + val.substring(3,val.length);
-            console.log(reallyVal)
-            return reallyVal
-          }else{
-            return val
-          }
-        }
-      },
+
       xAxis: {
         type: 'value',
         boundaryGap: [0, 0.01],
         splitLine:{
           show:false
-        }
+        },
+        axisLabel:{
+          show:false
+        },
+
       },
       yAxis: {
+        axisTick: {show: false},
         type: 'category',
-        data: ['公安局新区', '榆中县公安局', '皋兰县公安局', '永登县公安局', '红古区公安局', '安宁区公安局','西固区公安局','七里河区公安局','城关区公安局'],
+        data: ['新区公安局', '榆中县公安局', '皋兰县公安局', '永登县公安局', '红古区公安局', '安宁区公安局','西固区公安局','七里河区公安局','城关区公安局'],
+
+        axisLabel: {
+          formatter(val) {
+            return val.replace('公安局', '\n公安局');
+          }
+        }
+
 
       },
 
@@ -37,20 +39,24 @@ export const Chart2 = () => {
         {
           name: '破案排名1',
           type: 'bar',
-          data: [18203, 23489, 29034, 104970, 131744, 630230]
+          data: [1, 2, 3, 4, 5, 6, 7, 8, 9]
         },
         {
           name: '破案排名2',
           type: 'bar',
-          data: [19325, 23438, 31000, 121594, 134141, 681807]
+          data: [2, 3, 4, 5, 6, 7, 8, 9, 10]
         }
       ]
     }))},[])
 
   return (
-    <div className="bordered 管辖统计">
+    <div className="bordered 破获排名">
       <h2>案件破获排名</h2>
       <div ref={divRef}className="chart"></div>
+      <div className="legend">
+        <span className="first"/> 破案排名1
+        <span className="second"/> 破案排名1
+      </div>
     </div>
   )
 }
